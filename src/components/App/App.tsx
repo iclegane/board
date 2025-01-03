9d2ba6720fde0eb7a89aeb81a3251efedbd6167f
Viktor <vvs-PathOfExile@yandex.ru>
2025-01-03 20:28:55 +0300

    - added scale and backPosition

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,17 +6,19 @@ type PositionType = { x: number; y: number }
 export const App: React.FC = () => {
   const [position, setPosition] = useState<PositionType>({ x: 0, y: 0 })
   const [dragging, setDragging] = useState(false)
+  const [zoom, setZoom] = useState(1)
 
-  const lastPosition = useRef<PositionType>({ x: 0, y: 0 })
+  const lastClientPosition = useRef<PositionType>({ x: 0, y: 0 })
+  const lastCoordinatePosition = useRef<PositionType>({ x: 0, y: 0 })
 
   const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>): void => {
     setDragging(true)
-    lastPosition.current = { x: event.clientX, y: event.clientY }
+    lastClientPosition.current = { x: event.clientX, y: event.clientY }
+    lastCoordinatePosition.current = { ...position }
   }
 
-  const handleMouseUp = (event: React.MouseEvent<HTMLDivElement>): void => {
+  const handleMouseUp = (): void => {
     setDragging(false)
-    lastPosition.current = { x: event.clientX, y: event.clientY }
   }
 
   const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>): void => {
@@ -24,8 +26,8 @@ export const App: React.FC = () => {
       return
     }
 
-    const deltaX = event.clientX - lastPosition.current.x
-    const deltaY = event.clientY - lastPosition.current.y
+    const deltaX = event.clientX - lastClientPosition.current.x
+    const deltaY = event.clientY - lastClientPosition.current.y
 
     setPosition((prev) => {
       const newPosition = { x: prev.x + deltaX, y: prev.y + deltaY }
@@ -42,32 +44,92 @@ export const App: React.FC = () => {
       return newPosition
     })
 
-    lastPosition.current = { x: event.clientX, y: event.clientY }
+    lastClientPosition.current = { x: event.clientX, y: event.clientY }
   }
 
   const handleBackPosition = (): void => {
-    setPosition({ x: 0, y: 0 })
+    const { x, y } = lastCoordinatePosition.current
+    setPosition({ x, y })
 
-    document.documentElement.style.setProperty('--background-x', `${0}px`)
-    document.documentElement.style.setProperty('--background-y', `${0}px`)
+    document.documentElement.style.setProperty('--background-x', `${x}px`)
+    document.documentElement.style.setProperty('--background-y', `${y}px`)
+  }
+
+  const handleWheel = (event: React.WheelEvent<HTMLDivElement>): void => {
+    const cursorX = event.clientX
+    const cursorY = event.clientY
+
+    const newZoom = event.deltaY < 0 ? zoom + 0.1 : zoom - 0.1
+    const roundedZoom = Math.round(newZoom * 10) / 10 || 1
+
+    setPosition((prevPosition) => {
+      const deltaX = cursorX - prevPosition.x
+      const deltaY = cursorY - prevPosition.y
+
+      const newX = prevPosition.x + deltaX * (1 - roundedZoom / zoom)
+      const newY = prevPosition.y + deltaY * (1 - roundedZoom / zoom)
+
+      return { x: newX, y: newY }
+    })
+    setZoom(roundedZoom)
+
+    const backgroundScale = `${roundedZoom * 100}%`
+    document.documentElement.style.setProperty(
+      '--background-scale',
+      backgroundScale
+    )
   }
 
   return (
-    <div
-      className='board-container'
-      onMouseUp={handleMouseUp}
-      onMouseMove={handleMouseMove}
-      onMouseDown={handleMouseDown}
-    >
-      <button onClick={handleBackPosition}>Назад</button>
+    <div className='board'>
       <div
-        className='board-container-window'
+        className='board-wrapper'
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+        onMouseDown={handleMouseDown}
+        onWheel={handleWheel}
         style={{
           cursor: dragging ? 'grabbing' : 'grab',
-          transform: `translate(${position.x}px, ${position.y}px)`,
         }}
       >
-        <div className='board-content'>target</div>
+        <div className='actions'>
+          <button
+            onMouseDown={(e) => {
+              e.preventDefault()
+              e.stopPropagation()
+            }}
+            onClick={handleBackPosition}
+          >
+            Назад
+          </button>
+          <div>Zoom: {zoom}</div>
+        </div>
+        <div
+          className='board-wrapper-window'
+          style={{
+            transform: `translate(${position.x}px, ${position.y}px) scale(${zoom})`,
+            transformOrigin: 'center',
+          }}
+        >
+          <div
+            className='board-content'
+            style={{ transform: `translate(10px, 15px)` }}
+          >
+            target
+          </div>
+          <div
+            className='board-content'
+            style={{ transform: `translate(100px, 150px)` }}
+          >
+            target1
+          </div>
+          <div
+            className='board-content'
+            style={{ transform: `translate(320px, 125px)` }}
+          >
+            target2
+          </div>
+        </div>
       </div>
     </div>
   )
