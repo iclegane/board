6f5f2ba14e8711615749ed871052f864ad7a2341
Viktor <vvs-PathOfExile@yandex.ru>
2025-03-22 15:40:25 +0300

    added config

---
+++ server/src/config/index.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import process from "process";
+
+dotenv.config();
+
+// Todo: Добавить валидацию
+export const CONFIG = {
+    PORT : process.env.APP_PORT,
+    WS_PORT : process.env.WS_PORT,
+    MONGO_URI : process.env.MONGO_URI,
+    CLIENT_ORIGIN : process.env.CLIENT_ORIGIN,
+} as const;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import dotenv from "dotenv";
 import process from "process";
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
@@ -8,26 +7,17 @@ import Auth from './routes/Auth.js';
 import Board from './routes/Board.js';
 import { WSServer } from "./service/WS.js";
 import { Mongo } from "./service/Mongo.js";
-
-dotenv.config();
+import { CONFIG } from './config/index.js'
 
 const app = express();
-const PORT = process.env.APP_PORT;
-const WS_PORT = process.env.WS_PORT;
-const MONGO_URI = process.env.MONGO_URI;
-const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN;
 
-if (!PORT || !WS_PORT || !MONGO_URI || !CLIENT_ORIGIN) {
-    throw new Error("process.env vars is missing");
-}
-
-new Mongo(MONGO_URI);
-new WSServer(Number(WS_PORT));
+new Mongo(CONFIG.MONGO_URI);
+new WSServer(Number(CONFIG.WS_PORT));
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: CLIENT_ORIGIN,
+    origin: CONFIG.CLIENT_ORIGIN,
     credentials: true
 }));
 // Todo: Winston/Morgan
@@ -39,8 +29,8 @@ app.use((req, res, next) => {
 app.use('/api/v1/auth', Auth);
 app.use('/api/v1/board', Board);
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+app.listen(CONFIG.PORT, () => {
+    console.log(`Server running at http://localhost:${CONFIG.PORT}`);
 })
 
 process.on("SIGTERM", () => {
